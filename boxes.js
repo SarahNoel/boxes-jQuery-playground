28fd5897e60d24a99b9d4a0acd979237bef72dc6
Sarah Turek <snturek@gmail.com>
2015-08-03 10:27:17 -0600

    removed eval function

--- boxes.js
+++ boxes.js
@@ -27,11 +27,11 @@ $('#container').on("click", function(e){
   });
 
 // // Add links inside all red box divs that take the user to galvanize. Then add an on click handler that alerts the user that you can never leave the page. Make sure the user won't leave the page after the alert!
-// $('.boxType1').append('<a href ="www.galvanize.com">Galvanize</a>')
+$('.boxType1').append('<a href ="www.galvanize.com">Galvanize</a>')
 
-// $('.boxType1').on("click", function(e){
-//   e.preventDefault();
-//   alert("You can never leave this page!");});
+$('.boxType1').on("click", function(e){
+  e.preventDefault();
+  alert("You can never leave this page!");});
 
 // // For all box divs, add a click handler that adds an image of a cute puppy to the box. If the image is clicked again, remove the cute puppy.
 
@@ -43,13 +43,13 @@ $('.box').on("click", function(){
 
 // // Write a click handler on the conatainer div. The click handler should turn the container background to black and the background of the original div that was clicked to white. If the user original div that was clicked happened to be the container div, change the background of the container div to lime green. You should not use any selectors for this exercise. You can do it completely with what is given to you in the event callback.
 
-$("#container").click(function(event) {
-  if($("#container").is(event.target)) {
-     $("#container").css("background", "limegreen");
-  } else {
-    $("#container").css("background", "black");
-    $(event.target).css("background", "white");
-  }
-});
+// $("#container").click(function(event) {
+//   if($("#container").is(event.target)) {
+//      $("#container").css("background", "limegreen");
+//   } else {
+//     $("#container").css("background", "black");
+//     $(event.target).css("background", "white");
+//   }
+// });
 
 });
